467489ba5cfd1ac4e9efdea96db1f817d27384d2
eduschnell <eduschnell>
2006-03-26 03:32:40 +0000

    no message
    --HG--
    extra : convert_revision : ff01285d889629b02a65d471255c66b1e47fef20

--- datasoul-v1/src/datasoul/templates/ImageTemplateItem.java
+++ datasoul-v1/src/datasoul/templates/ImageTemplateItem.java
@@ -9,13 +9,32 @@
 
 package datasoul.templates;
 
+import com.sun.image.codec.jpeg.ImageFormatException;
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGImageDecoder;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+import com.sun.imageio.plugins.png.PNGImageReader;
 import java.awt.AlphaComposite;
 import java.awt.Composite;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.Raster;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageInputStream;
+import javax.swing.ImageIcon;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  *
@@ -24,6 +43,7 @@ import javax.imageio.ImageIO;
 public class ImageTemplateItem extends TemplateItem {
     
     BufferedImage img;
+    private String imageInStr;
     private String filename;
     
     public ImageTemplateItem() {
@@ -33,13 +53,27 @@ public class ImageTemplateItem extends TemplateItem {
     /** Creates a new instance of ImageTemplateItem */
     public ImageTemplateItem(String filename) {
         super();
-        this.setFilename(filename);       
+        this.loadImage(filename);       
     }
 
+    private void loadImage(String filename){
+        if (filename != null){
+            this.filename = filename;
+            try {
+
+                img = ImageIO.read( new File(filename) );
+                this.setWidth( img.getWidth() );
+                this.setHeight( img.getHeight() );
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    
+    
     @Override
     protected void registerProperties(){
         super.registerProperties();
-        properties.add("Filename");
     }
     
     @Override
@@ -55,23 +89,76 @@ public class ImageTemplateItem extends TemplateItem {
         
     }
 
-    public String getFilename() {
-        return filename;
-    }
-
-    public void setFilename(String filename) {
-        if (filename != null){
-            this.filename = filename;
-            try {
-                img = ImageIO.read( new File(filename) );
-                this.setWidth( img.getWidth() );
-                this.setHeight( img.getHeight() );
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-            firePropChanged("Filename");
+    public String getImageInStr() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            ImageIO.write( this.img, "png", baos);
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
+        
+        byte[] ba = baos.toByteArray();
+
+        int len=ba.length,j;
+        StringBuffer sb=new StringBuffer(len*2);
+        for (j=0;j<len;j++) {
+            String sByte=Integer.toHexString((int)(ba[j] & 0xFF));
+            sb.append(stringAlign2chars(sByte));
+        }
+        return sb.toString();
+
     }
 
+    private String stringAlign2chars(String str){
+        if (str.length()!=2)
+            return '0'+str;
+        else
+            return str;
+    }
+    
+    private String stringAlign4chars(String str){
+        return ("0000".substring(0,4-str.length()))+str;
+    }    
+    public void setImageInStr(String strImage) {
+
+        String str="";
+        int intAux=0;
+        byte[] bytes = new byte[strImage.length()/2];
+        for(int i=0; i< strImage.length()-1;i=i+2){
+            str = strImage.substring(i,i+2);
+            intAux = Integer.parseInt(str,16);
+            bytes[i/2]=(byte)intAux;
+        }
+        
+        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+        try {
+            this.img = ImageIO.read(bais);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+    }        
+    
+     public Node writeObject() throws Exception{
+
+        properties.add("ImageInStr");
+              
+        Node node = super.writeObject();
+        
+        properties.remove("ImageInStr");
+        
+        return node;
+     }
+     
+     public void readObject(Node nodeIn) throws Exception {
+
+        properties.add("ImageInStr");
+              
+        super.readObject(nodeIn);
+        
+        properties.remove("ImageInStr");
+        
+        return;
+     }
     
 }
