34270524574feea15517af1f1c31c79069000e6c
svmello <svmello>
2008-02-09 23:25:59 +0000

    Build environment
    --HG--
    extra : convert_revision : d61429e9b687f434429ed3d08738073d6abeb0f1

---
+++ datasoul-v1/packaging/build-all.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+
+DSVERSION=$(cat datasoul.xml | awk -F\< "/<version>/{ print \$2 }" | awk -F\> "{ print \$2}")
+rm -Rf datasoul-$DSVERSION
+
+# Clean up
+rm -Rf installers
+ant clean 
+find ../src -name "*#*" | xargs rm -f
+
+# Compile
+ant jar
+
+# Build installers
+/opt/installbuilder-5.4.1/bin/builder build datasoul.xml linux 
+/opt/installbuilder-5.4.1/bin/builder build datasoul.xml windows
+/opt/installbuilder-5.4.1/bin/builder build datasoul.xml osx 
+/opt/installbuilder-5.4.1/bin/builder build datasoul.xml rpm
+/opt/installbuilder-5.4.1/bin/builder build datasoul.xml deb
+
+# Clean up
+ant clean
+
+# Create source tarball
+mkdir datasoul-$DSVERSION
+mkdir datasoul-$DSVERSION/src
+cp  -r ../src/datasoul  datasoul-$DSVERSION/src
+find datasoul-$DSVERSION/src -name "CVS" | xargs rm -Rf
+tar czvf installers/datasoul-$DSVERSION-source.tar.gz datasoul-$DSVERSION
+rm -Rf datasoul-$DSVERSION
+
+# Compress MacOS X installer
+cd installers
+OSX=$(ls -1 | grep osx)
+zip -r $OSX.zip $OSX
+rm -Rf $OSX
+cd ..
