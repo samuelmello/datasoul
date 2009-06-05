33f16ee24c3e89fda1c28d2467100090dd69c588
Samuel Lucas Vaz de Mello <svmello@users.sourceforge.net>
2009-06-05 00:06:53 -0300

    Updated debian package creation
    --HG--
    extra : convert_revision : 11faa89ea579f1b6fa0f074e70d7ab12080944db

--- datasoul-v1/packaging/build-all.sh
+++ datasoul-v1/packaging/build-all.sh
@@ -9,6 +9,7 @@ rm -Rf datasoul-$DSVERSION
 rm -Rf installers
 ant clean 
 find ../src -name "*#*" | xargs rm -f
+mkdir installers
 
 # Setup vesion
 echo "version=$DSVERSION" > ../src/datasoul/version.properties
@@ -21,6 +22,16 @@ ant jar
 # Build windows 
 #BUILDER_CMD=/opt/installbuilder-5.4.10/bin/builder
 #${BUILDER_CMD} build datasoul.xml windows
+
+# Build debian
+mkdir -p debian/datasoul/usr/lib/datasoul
+cd debian
+sed -i "s/Version:.*/Version: ${DSVERSION}/" datasoul/DEBIAN/control
+cp -r ../dist/* datasoul/usr/lib/datasoul
+dpkg -b datasoul
+mv datasoul.deb ../installers/datasoul_${DSVERSION}_all.deb
+rm -Rf datasoul/usr/lib/datasoul/*
+cd ..
 
 # Build MacOSX
 MACINSTDIR=installers/Datasoul-${DSVERSION}-MacOSX/Datasoul-${DSVERSION}.app
