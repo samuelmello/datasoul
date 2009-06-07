3afd2264793f855079c2f7360569838050207b58
Samuel Lucas Vaz de Mello <svmello@users.sourceforge.net>
2009-06-07 01:15:50 -0300

    Improved build script
    --HG--
    extra : convert_revision : 2d4240b4b7e04a5266b26dffbe2d4d3784d4ad09

--- datasoul-v1/packaging/build-all.sh
+++ datasoul-v1/packaging/build-all.sh
@@ -1,24 +1,21 @@
 #!/bin/bash -x
 
 DSVERSION=$(awk -F= '/version/{ print $2 }' ../src/datasoul/version.properties)
-rm -Rf datasoul-$DSVERSION
 
 ##################################################
 
 # Clean up
 rm -Rf installers
 ant clean 
-find ../src -name "*#*" | xargs rm -f
 mkdir installers
 
 # Compile
 ant jar
 
-##################################################
+# Remove generated README.TXT file
+rm -f dist/README.TXT
 
-# Build windows 
-#BUILDER_CMD=/opt/installbuilder-5.4.10/bin/builder
-#${BUILDER_CMD} build datasoul.xml windows
+##################################################
 
 # Build debian
 mkdir -p debian/datasoul/usr/lib/datasoul
@@ -45,7 +42,7 @@ zip -r Datasoul-${DSVERSION}-MacOSX.zip Datasoul-${DSVERSION}-MacOSX
 cd ..
 rm -Rf installers/Datasoul-${DSVERSION}-MacOSX
 
-# Windows
+# Build Windows
 makensis windows.nsi
 mv Datasoul-${DSVERSION}.exe installers
 
@@ -60,5 +57,4 @@ rm -Rf datasoul-$DSVERSION
 ###################################################
 # Clean up
 ant clean
-
 
