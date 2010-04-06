0844d96c02074fa41c1d9f1ec934ab63f867feb7
Samuel Lucas Vaz de Mello <samuel@samuel-desktop.(none)>
2010-04-05 22:00:41 -0300

    Implemented Chord Transposition
    --HG--
    extra : convert_revision : 2802617007a3171515e71063341cf782b9851831

--- datasoul-v1/src/datasoul/serviceitems/song/SongChordEditorPanel.java
+++ datasoul-v1/src/datasoul/serviceitems/song/SongChordEditorPanel.java
@@ -12,13 +12,24 @@
 package datasoul.serviceitems.song;
 
 import java.util.ArrayList;
-import javax.swing.JPanel;
+import java.util.HashMap;
+import java.util.regex.Matcher;
 
 /**
  *
  * @author samuel
  */
 public class SongChordEditorPanel extends javax.swing.JPanel {
+
+    private enum TransposeDirection{
+        Transpose_Up,
+        Transpose_Down
+    };
+
+    private static HashMap<String, String> mapTranspUpFlat;
+    private static HashMap<String, String> mapTranspUpSharp;
+    private static HashMap<String, String> mapTranspDownFlat;
+    private static HashMap<String, String> mapTranspDownSharp;
 
     /** Creates new form SongChordEditorPanel */
     public SongChordEditorPanel() {
@@ -50,8 +61,8 @@ public class SongChordEditorPanel extends javax.swing.JPanel {
         jLabel3 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox();
-        jButton3 = new javax.swing.JButton();
+        cbTranspose = new javax.swing.JComboBox();
+        btnTransposeUp = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
 
         setLayout(new java.awt.BorderLayout());
@@ -118,20 +129,30 @@ public class SongChordEditorPanel extends javax.swing.JPanel {
         jLabel4.setText("  ");
         transposeToolbar.add(jLabel4);
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "#", "b" }));
-        jComboBox1.setMaximumSize(new java.awt.Dimension(50, 32767));
-        transposeToolbar.add(jComboBox1);
+        cbTranspose.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "#", "b" }));
+        cbTranspose.setMaximumSize(new java.awt.Dimension(50, 32767));
+        transposeToolbar.add(cbTranspose);
 
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/datasoul/icons/v2/view-sort-ascending.png"))); // NOI18N
-        jButton3.setFocusable(false);
-        jButton3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButton3.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        transposeToolbar.add(jButton3);
+        btnTransposeUp.setIcon(new javax.swing.ImageIcon(getClass().getResource("/datasoul/icons/v2/view-sort-ascending.png"))); // NOI18N
+        btnTransposeUp.setFocusable(false);
+        btnTransposeUp.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnTransposeUp.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        btnTransposeUp.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnTransposeUpActionPerformed(evt);
+            }
+        });
+        transposeToolbar.add(btnTransposeUp);
 
         jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/datasoul/icons/v2/view-sort-descending.png"))); // NOI18N
         jButton2.setFocusable(false);
         jButton2.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         jButton2.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         transposeToolbar.add(jButton2);
 
         add(transposeToolbar, java.awt.BorderLayout.NORTH);
@@ -146,6 +167,14 @@ public class SongChordEditorPanel extends javax.swing.JPanel {
         cmf.setLocationRelativeTo(this);
         cmf.setVisible(true);
     }//GEN-LAST:event_btnTabsCatalogActionPerformed
+
+    private void btnTransposeUpActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTransposeUpActionPerformed
+        transposeChords(TransposeDirection.Transpose_Up);
+    }//GEN-LAST:event_btnTransposeUpActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        transposeChords(TransposeDirection.Transpose_Down);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
 
     public void init(){
@@ -207,13 +236,224 @@ public class SongChordEditorPanel extends javax.swing.JPanel {
 
     }
 
+    private static synchronized void initTransposeMaps(){
+
+        mapTranspUpFlat = new HashMap<String,String>();
+        mapTranspUpSharp = new HashMap<String,String>();
+        mapTranspDownFlat = new HashMap<String,String>();
+        mapTranspDownSharp = new HashMap<String,String>();
+
+        // Up using flats
+        mapTranspUpFlat.put("Ab", "A" );
+        mapTranspUpFlat.put("A",  "Bb");
+        mapTranspUpFlat.put("A#", "B" );
+        mapTranspUpFlat.put("Bb", "B" );
+        mapTranspUpFlat.put("B",  "C" );
+        mapTranspUpFlat.put("B#", "Db");
+        mapTranspUpFlat.put("Cb", "C" );
+        mapTranspUpFlat.put("C",  "Db");
+        mapTranspUpFlat.put("C#", "D" );
+        mapTranspUpFlat.put("Db", "D" );
+        mapTranspUpFlat.put("D",  "Eb");
+        mapTranspUpFlat.put("D#", "E" );
+        mapTranspUpFlat.put("Eb", "E" );
+        mapTranspUpFlat.put("E",  "F" );
+        mapTranspUpFlat.put("E#", "Gb");
+        mapTranspUpFlat.put("Fb", "F" );
+        mapTranspUpFlat.put("F",  "Gb");
+        mapTranspUpFlat.put("F#", "G" );
+        mapTranspUpFlat.put("Gb", "G" );
+        mapTranspUpFlat.put("G",  "Ab");
+        mapTranspUpFlat.put("G#", "A" );
+
+        // Up using Sharps
+        mapTranspUpSharp.put("Ab", "A" );
+        mapTranspUpSharp.put("A",  "A#");
+        mapTranspUpSharp.put("A#", "B" );
+        mapTranspUpSharp.put("Bb", "B" );
+        mapTranspUpSharp.put("B",  "C" );
+        mapTranspUpSharp.put("B#", "C#");
+        mapTranspUpSharp.put("Cb", "C" );
+        mapTranspUpSharp.put("C",  "C#");
+        mapTranspUpSharp.put("C#", "D" );
+        mapTranspUpSharp.put("Db", "D" );
+        mapTranspUpSharp.put("D",  "D#");
+        mapTranspUpSharp.put("D#", "E" );
+        mapTranspUpSharp.put("Eb", "E" );
+        mapTranspUpSharp.put("E",  "F" );
+        mapTranspUpSharp.put("E#", "F#");
+        mapTranspUpSharp.put("Fb", "F" );
+        mapTranspUpSharp.put("F",  "F#");
+        mapTranspUpSharp.put("F#", "G" );
+        mapTranspUpSharp.put("Gb", "G" );
+        mapTranspUpSharp.put("G",  "G#");
+        mapTranspUpSharp.put("G#", "A" );
+
+        // Down using flats
+        mapTranspDownFlat.put("Ab", "G" );
+        mapTranspDownFlat.put("A",  "Ab");
+        mapTranspDownFlat.put("A#", "A" );
+        mapTranspDownFlat.put("Bb", "A" );
+        mapTranspDownFlat.put("B",  "Bb");
+        mapTranspDownFlat.put("B#", "B" );
+        mapTranspDownFlat.put("Cb", "Bb");
+        mapTranspDownFlat.put("C",  "B" );
+        mapTranspDownFlat.put("C#", "C" );
+        mapTranspDownFlat.put("Db", "C" );
+        mapTranspDownFlat.put("D",  "Db");
+        mapTranspDownFlat.put("D#", "D" );
+        mapTranspDownFlat.put("Eb", "D" );
+        mapTranspDownFlat.put("E",  "Eb");
+        mapTranspDownFlat.put("E#", "E" );
+        mapTranspDownFlat.put("Fb", "Eb");
+        mapTranspDownFlat.put("F",  "E" );
+        mapTranspDownFlat.put("F#", "F" );
+        mapTranspDownFlat.put("Gb", "F" );
+        mapTranspDownFlat.put("G",  "Gb");
+        mapTranspDownFlat.put("G#", "G" );
+
+        // Down using sharps
+        mapTranspDownSharp.put("Ab", "G" );
+        mapTranspDownSharp.put("A",  "G#");
+        mapTranspDownSharp.put("A#", "A" );
+        mapTranspDownSharp.put("Bb", "A" );
+        mapTranspDownSharp.put("B",  "A#");
+        mapTranspDownSharp.put("B#", "B" );
+        mapTranspDownSharp.put("Cb", "A#");
+        mapTranspDownSharp.put("C",  "B" );
+        mapTranspDownSharp.put("C#", "C" );
+        mapTranspDownSharp.put("Db", "C" );
+        mapTranspDownSharp.put("D",  "C#");
+        mapTranspDownSharp.put("D#", "D" );
+        mapTranspDownSharp.put("Eb", "D" );
+        mapTranspDownSharp.put("E",  "D#");
+        mapTranspDownSharp.put("E#", "E" );
+        mapTranspDownSharp.put("Fb", "D#");
+        mapTranspDownSharp.put("F",  "E" );
+        mapTranspDownSharp.put("F#", "F" );
+        mapTranspDownSharp.put("Gb", "F" );
+        mapTranspDownSharp.put("G",  "F#");
+        mapTranspDownSharp.put("G#", "G" );
+
+
+    }
+
+    private String transposeSingleChord(TransposeDirection direction, String from){
+
+        HashMap<String,String> map;
+
+        if (mapTranspDownFlat == null){
+            SongChordEditorPanel.initTransposeMaps();
+        }
+
+        // sharp or flags
+        if ( cbTranspose.getSelectedItem().toString().equals("#")){
+            if (direction == TransposeDirection.Transpose_Up){
+                map = SongChordEditorPanel.mapTranspUpSharp;
+            }else{
+                map = SongChordEditorPanel.mapTranspDownSharp;
+            }
+        }else{
+            if (direction == TransposeDirection.Transpose_Up){
+                map = SongChordEditorPanel.mapTranspUpFlat;
+            }else{
+                map = SongChordEditorPanel.mapTranspDownFlat;
+            }
+        }
+
+        if (map.containsKey(from)){
+            return map.get(from);
+        }else{
+            return from;
+        }
+
+    }
+
+    private void transposeLine(String line, StringBuffer output, TransposeDirection direction){
+
+        int addspace = 0;
+        int readsize = 0;
+        String transposed;
+
+        // First, expand any tabs
+        line = line.replaceAll("\t", "        ");
+
+        for (int i=0; i<line.length(); i++){
+
+            // Need to transpose
+            if (line.charAt(i) >= 'A' && line.charAt(i) <= 'G'){
+
+                // Check for # or b
+                if ((i+1 < line.length()) && (line.charAt(i+1)=='b' || line.charAt(i+1)=='#')){
+                    readsize = 2;
+                    transposed = transposeSingleChord(direction, Character.toString(line.charAt(i)) + Character.toString(line.charAt(i+1)) );
+                    i++;
+                }else{
+                    readsize = 1;
+                    transposed = transposeSingleChord(direction, Character.toString(line.charAt(i)) );
+                }
+
+                output.append(transposed);
+
+                // If needed add or remove blank spaces
+                if (readsize == 2 && transposed.length() == 1){
+                    addspace++;
+                }else if (readsize == 1 && transposed.length() == 2){
+                    addspace--;
+                }
+            } else if (addspace != 0 && line.charAt(i)==' '){
+
+                // Check for spaces
+                if (addspace < 0){
+                    addspace++;
+                    continue;
+                }
+
+                while (addspace > 0){
+                    output.append(" ");
+                    addspace--;
+                }
+                
+                output.append(" ");
+                
+            } else {
+                // Copy any other char
+                output.append(line.charAt(i));
+            }
+
+        }// for each char
+    }
+
+
+    private void transposeChords(TransposeDirection direction){
+
+        int pos;
+
+        StringBuffer output = new StringBuffer();
+        for (String line : textChords.getText().split("\n")){
+            Matcher m = Song.CHORDS_REGEX_PATTERN.matcher(line);
+            if (m.find()){
+                transposeLine(line, output, direction);
+            }else{
+                output.append(line);
+            }
+            output.append("\n");
+        }
+
+        pos = textChords.getCaretPosition();
+
+        textChords.setText(output.toString());
+
+        textChords.setCaretPosition(pos);
+
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnRefreshTabs;
     private javax.swing.JButton btnTabsCatalog;
+    private javax.swing.JButton btnTransposeUp;
+    private javax.swing.JComboBox cbTranspose;
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
