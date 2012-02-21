4f2b060a9c0d285c47189830ae7ec993b40d0590
Samuel Mello <samuelmello@gmail.com>
2012-02-21 16:17:27 -0200

    Added copyright notes to new files

--- datasoul-src/src/datasoul/render/remote/RemoteLauncherDialog.java
+++ datasoul-src/src/datasoul/render/remote/RemoteLauncherDialog.java
@@ -1,12 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * RemoteLauncherDialog.java
+/* 
+ * Copyright 2012 Samuel Mello
  *
- * Created on Sep 13, 2010, 11:10:15 PM
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; version 2 of the License.
+ * 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * 
  */
 
 package datasoul.render.remote;
