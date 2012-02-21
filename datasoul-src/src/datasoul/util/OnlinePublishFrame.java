ef51626cb0bfe7f250583bec059789ff56b9795e
Samuel Mello <samuelmello@gmail.com>
2012-02-21 18:22:24 -0200

    Changed license to GPLv2-only to GPLv2 or later to allow linking with GPLv3 and AGPL packages.

--- datasoul-src/src/datasoul/util/OnlinePublishFrame.java
+++ datasoul-src/src/datasoul/util/OnlinePublishFrame.java
@@ -1,13 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2005-2012 Samuel Mello
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; version 2 or later of the License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
  */
 
-/*
- * OnlinePublishFrame.java
- *
- * Created on 17/06/2011, 23:39:12
- */
+
 package datasoul.util;
 
 import java.net.URI;
